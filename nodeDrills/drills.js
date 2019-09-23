e60d55f064f3fa1a963119f34372e7bc278492ae
Joey Romo <joeyjr95@gmail.com>
2019-09-23 15:42:06 -0400

    second drill

--- nodeDrills/drills.js
+++ nodeDrills/drills.js
@@ -9,10 +9,27 @@ app.get('/sum', (req, res)=>{
   if ( !req.query.a || !req.query.b){
     return res.status(400).send('must include parameters');
   }
-//   else if((typeof req.query.a !== 'number') ||(typeof req.query.b !== 'number')){
-//     return res.status(400).send('must include numbers');
-//   }
-  return res.send(`The sum of ${req.query.a} and ${req.query.b} is ${parseInt(req.query.a, 10) + parseInt(req.query.b, 10)}`)
+  //   else if((typeof req.query.a !== 'number') ||(typeof req.query.b !== 'number')){
+  //     return res.status(400).send('must include numbers');
+  //   }
+  return res.send(`The sum of ${req.query.a} and ${req.query.b} is ${parseInt(req.query.a, 10) + parseInt(req.query.b, 10)}`);
+});
+
+app.get('/cipher', (req, res)=> {
+  if ( !req.query.text || !req.query.shift){
+    return res.status(400).send('must include parameters');
+  }
+  let encodedString ='';
+  for( let i = 0; i < req.query.text.length; i++){
+    if (req.query.text.charCodeAt(i) +  parseInt(req.query.shift, 10) > 90){
+      encodedString += String.fromCharCode(req.query.text.charCodeAt(i) + parseInt(req.query.shift, 10) - 26);
+    }
+    else{
+      encodedString += String.fromCharCode(req.query.text.charCodeAt(i) + parseInt(req.query.shift, 10));
+    }
+  } 
+  return res.send( encodedString);
+
 });
 
 
